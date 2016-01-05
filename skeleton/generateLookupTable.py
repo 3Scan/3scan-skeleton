90ab008241d2995fcdcb87d5ebd2a7d3e1bb5ae4
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-01-05 17:20:18 -0600

    lookup table generation for all the 12 orientations

--- skeleton/generateLookupTable.py
+++ skeleton/generateLookupTable.py
@@ -1,107 +1,43 @@
 import numpy as np
-
-from pyeda.inter import exprvar
-
-"""
-   To avoid pyeda overhead and evaluating the
-   expressions each time,get a boolean
-   expression in advance and evaluate it
-"""
+from rotationalOperators import firstSubIter, secondSubIter, thirdSubIter, fourthSubIter, fifthSubIter, sixthSubIter
+from rotationalOperators import seventhSubIter, eighthSubIter, ninthSubIter, tenthSubIter, eleventhSubIter, twelvethSubIter
 
 
-def emitCodeFromEqtn(eqtn, eqName):
-    simplified = eqtn.simplify()
-    uniqueIDtoSymbol = {symbol.uniqid: symbol for symbol in eqtn.inputs}
-
-    # args = ", ".join(["uint8 %s" % symbol.name for symbol in eqtn.inputs])
-
-    # Emit the c function header
-    # print("uint8 %s(%s) {" % (eqName, args))
-    statement = recursiveEmitter(simplified.to_ast(), uniqueIDtoSymbol)
-    # print("\treturn %s;" % statement)
-    # print("}")
-    return statement
-
-
-def recursiveEmitter(ast, symbolTable):
-    # Input ast format is (operator, expr, expr, expr, expr. . .)
-    op = ast[0]
-    exprs = ast[1:]
-
-    if op == "and":
-        subExprs = [recursiveEmitter(exp, symbolTable) for exp in exprs]
-        return "(" + " & ".join(subExprs) + ")"
-
-    elif op == "or":
-        subExprs = [recursiveEmitter(exp, symbolTable) for exp in exprs]
-        return "(" + " | ".join(subExprs) + ")"
-
-    elif op == "not":
-        subExprs = [recursiveEmitter(exp, symbolTable) for exp in exprs]
-        return "(" + " not ".join(subExprs) + ")"
-
-    elif op == "xor":
-        subExprs = [recursiveEmitter(exp, symbolTable) for exp in exprs]
-        return "(" + " ^ ".join(subExprs) + ")"
-
-    elif op == "lit":
-        # Lit can have only one operator
-        symbol = exprs[0]
-
-        if symbol < 0:
-            symbol *= -1
-            return "(not %s)" % symbolTable[symbol].name
+def generateLookuparray(stop, iterationNumber):
+    lookuparray = np.zeros(stop, dtype=np.uint8)
+    print(iterationNumber)
+    for item in range(0, stop):
+        neighborValues = [(item >> digit) & 0x01 for digit in range(26)]
+        neighborValues.insert(13, 1)
+        neighborMatrix = np.reshape(neighborValues, (3, 3, 3))
+        if np.sum(neighborValues) == 1:
+            lookuparray[item] = 0
         else:
-            return symbolTable[symbol].name
-    raise RuntimeError("No way to resolve operation named '%s' with %i arguments" % (op, len(exprs)))
+            neighborValues = iterationNumber(neighborMatrix)
+            a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = tuple(neighborValues)
+            shouldVoxelBedeleted = (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & p & (d | e | f | m | n | u | v | w | g | h | i | o | q | x | y | z)) | \
+                                   (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & v & (r | s | t | j | k | l | m | n | u | w | o | p | q | x | y | z)) | \
+                                   (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & y & (m | n | u | w | o | q | x | z)) | \
+                                   (~(a) & ~(b) & ~(c) & ~(k) & ~(e) & ~(d & j) & ~ (l & f) & p & v) | \
+                                   (~(a) & ~(b) & ~(k) & ~(e) & c & v & p & ~(j & d) & (l ^ f)) | \
+                                   (a & v & p & ~(b) & ~(c) & ~(k) & ~(e) & ~(l & f) & (j ^ d)) | \
+                                   (~(a) & ~(b) & ~(k) & ~(e) & n & v & p & ~(j & d)) | \
+                                   (~(b) & ~(c) & ~(k) & ~(e) & m & v & p & ~(l & f)) | \
+                                   (~(b) & ~(k) & ~(e) & a & n & v & p & (j ^ d)) | \
+                                   (~(b) & ~(k) & ~(e) & c & m & v & p & (l ^ f)) | \
+                                   (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(d) & ~(e) & ~(g) & ~(h) & q & y) | \
+                                   (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(e) & ~(f) & ~(h) & ~(i) & o & y) | \
+                                   (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(r) & ~(s) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & w & y) | \
+                                   (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & ~(k) & ~(l) & ~(s) & ~(t) & u & y)
+            lookuparray[item] = shouldVoxelBedeleted
+    return lookuparray
 
 
-def getExpression():
-    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = map(exprvar, 'abcdefghijklmnopqrstuvwxyz')
-    usDeletiondirection = (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & p & (d | e | f | m | n | u | v | w | g | h | i | o | q | x | y | z)) | \
-                          (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & v & (r | s | t | j | k | l | m | n | u | w | o | p | q | x | y | z)) | \
-                          (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & y & (m | n | u | w | o | q | x | z)) | \
-                          (~(a) & ~(b) & ~(c) & ~(k) & ~(e) & ~(d & j) & ~ (l & f) & p & v) | \
-                          (~(a) & ~(b) & ~(k) & ~(e) & c & v & p & ~(j & d) & (l ^ f)) | \
-                          (a & v & p & ~(b) & ~(c) & ~(k) & ~(e) & ~(l & f) & (j ^ d)) | \
-                          (~(a) & ~(b) & ~(k) & ~(e) & n & v & p & ~(j & d)) | \
-                          (~(b) & ~(c) & ~(k) & ~(e) & m & v & p & ~(l & f)) | \
-                          (~(b) & ~(k) & ~(e) & a & n & v & p & (j ^ d)) | \
-                          (~(b) & ~(k) & ~(e) & c & m & v & p & (l ^ f)) | \
-                          (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(d) & ~(e) & ~(g) & ~(h) & q & y) | \
-                          (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(e) & ~(f) & ~(h) & ~(i) & o & y) | \
-                          (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(r) & ~(s) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & w & y) | \
-                          (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & ~(k) & ~(l) & ~(s) & ~(t) & u & y)
-    us = emitCodeFromEqtn(usDeletiondirection, usDeletiondirection)
-    return us
+directionList = [firstSubIter, secondSubIter, thirdSubIter, fourthSubIter,
+                 fifthSubIter, sixthSubIter, seventhSubIter, eighthSubIter,
+                 ninthSubIter, tenthSubIter, eleventhSubIter, twelvethSubIter]
 
-
-us = getExpression()
-# print(us)
-
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-
-def generateLookupTablearray(start, stop):
-    lookupTablearray = np.zeros(start:stop, dtype=np.uint8)
-    for i, value in enumerate(lookupTablearray):
-        if i > 40000 :
-            break
-        # Don't re-generate values we've already calculated.
-        neighborValues = [(i >> digit) & 0x01 for digit in range(26)]
-        if sum(neighborValues) == 1:
-            lookupTablearray[i] = 0
-        else:
-            neighborDict = dict(zip(alphabet, neighborValues))
-            lookupTablearray[i] = eval(us, neighborDict)
-    return lookupTablearray
-
-
-def main():
-    lookupTablearray = generateLookupTablearray()
-    np.save("lookupTablearray.npy", lookupTablearray)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    for i, item in enumerate(directionList):
+        lookuparray = generateLookuparray(2 ** 26, item)
+        np.save("lookuparray%i.npy" % (i + 1), lookuparray)
