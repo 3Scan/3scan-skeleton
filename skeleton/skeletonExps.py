04401e19351221637cec78d6e2302aafced776bf
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-02-22 16:51:09 -0600

    commented code that does simple displays

--- skeleton/skeletonExps.py
+++ skeleton/skeletonExps.py
@@ -25,9 +25,10 @@ for grey in subVolumes:
     maxip = np.amax(grey, 0)
     imsave('/media/pranathi/A336-5F43/greyImMaxip%i.png' % j, maxip)
     t = threshold_otsu(grey)
-    strLists.append("threshold of {} subvolume is {}". format(j, t))
     thresh = grey > t
     maskOut[thresh == 1] = 200
+    skeleton = getSkeletonize3D(thresh)
+    strLists.append("threshold of {} subvolume is {}". format(j, t))
     np.save("/media/pranathi/A336-5F43/thresholded%i.npy" % j, np.uint8(thresh))
     bound = _getBouondariesOfimage(thresh)
     np.save("/media/pranathi/A336-5F43/boundary%i.npy" % j, np.uint8(bound))
@@ -36,7 +37,6 @@ for grey in subVolumes:
     maxip = np.amax(thresh, 0)
     imsave('/media/pranathi/A336-5F43/threshImMaxip%i.png' % j, maxip * 255)
     st = time.time()
-    skeleton = getSkeletonize3D(thresh)
     sh = time.time()
     strLists.append("skeletonized %i number of pixels in %0.2f seconds" % (np.sum(thresh), sh - st))
     shortest = getShortestPathskeleton(skeleton)
@@ -65,3 +65,90 @@ for grey in subVolumes:
 
 f.writelines(strLists)
 f.close()
+
+
+# import itertools
+# import os
+# import time
+# import numpy as np
+# from scipy.misc import imread
+# from scipy import ndimage
+# import matplotlib.pyplot as plt
+# import matplotlib
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# rect1 = matplotlib.patches.Rectangle((1600, 0), 800, 17480, color='white')
+# rect2 = matplotlib.patches.Rectangle((4800, 0), 500, 17480, color='white')
+# ax.add_patch(rect1)
+# ax.add_patch(rect2)
+# plt.xlim([0, 8026])
+# plt.ylim([17480, 0])
+# plt.show()
+
+# verticalBlur = list(range(1600, 2400))
+# verticalBlur = verticalBlur + list(range(4800, 5300))
+
+# # valid 3D coordinates inside the brain
+# mask = np.load('/media/pranathi/DATA/maskDownsampled.npy')
+# aspectRatio = [140, 140]
+# listNzi = []; z = []; y = []; x = [];
+# for i in range(mask.shape[0]):
+#     zoomedOut = ndimage.interpolation.zoom(mask[i], zoom=aspectRatio, order=3, prefilter=False)
+#     y_sample, x_sample = np.array(np.where(zoomedOut != 0))
+#     y.append(y_sample); x.append(x_sample)
+#     # if a index is not in vertical blur i.e 1600 to 2400 and 4800 to 5300 on x axis continue
+#     # python - z, y, x z = 799 y = 0 to 17480 x = 0 to 8026
+#     valid = [tuple((j, l[0], l[1])) for j in range(0, 20 * (i + 1)) for l in map(list, np.transpose(np.nonzero(zoomedOut))) if l[1] not in verticalBlur]
+#     z = z + list(range(0, 20 * (i + 1)))
+#     listNzi = listNzi + valid
+
+
+# def _intersect(a, b):
+#     """
+#        return the intersection of two lists
+#     """
+#     if len(set(a) - set(b)) == 0:
+#         return 1
+#     else:
+#         return 0
+
+
+# # 8 increment corners of a sample
+# stepDirect = itertools.product((0, 128), repeat=3)
+# listStepDirect = list(stepDirect)
+
+# # if all of the 8 corners of the cube are inside the brain i.e listNzi then it is a valid sample
+# # check if i < 8026 / 128 and j < 17480 / 126 and k < 799 / 128
+
+
+# z_sample, y_sample, x_sample = np.array(np.where(mask != 0))
+# z = z_sample * 20; y = y_sample * 140; x = x_sample * 140
+# validCoordinates = [tuple((k, j, i)) for i, j, k in zip(x, y, z) if i not in verticalBlur]
+# assert len(validCoordinates) < len(z)
+# i = 0; j = 0; k = 0; sample = 0;
+# while i < 63 and j < 137 and k < 44:
+#     if _intersect(listStepDirect, listNzi):
+#         sample += 1
+
+# startt = time.time()
+# count = 0
+# root = input("please enter a root directory where your 2D slices are----")
+# formatOfFiles = input("please enter the format of 2D files---")
+# # list and sort all the files in the given greyscale
+# # input root directory
+# # which is to be skeletonized
+# listOfJpgs = [os.path.join(root, f) for f in os.listdir(root) if formatOfFiles in f]
+# count1 = 0
+# inputIm = np.zeros((19, 512, 512), dtype=np.uint8)
+# for fileName in subList:
+#     inputIm[count1][:][:] = imageExtract
+#     count1 += 1
+# for i in range(0, 63):
+#     r = ndimage.interpolation.zoom(inputIm[:, 0:128, 0: 128 * (i + 1)], [7, 1, 1], order=3, prefilter=False)
+
+
+# for i in range(0, len(l)):
+#     image = imread((os.path.join(root, 'thresh%i.png' %i)))
+#     imsave(root + 'rotatedThresh/' + 'binaryIm%i.png' % i, np.rot90(image, 3))
+
