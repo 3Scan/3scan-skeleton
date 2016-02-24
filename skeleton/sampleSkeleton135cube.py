163c6486984dce1576b9bfe802b6b1659efa96f8
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-02-24 17:14:17 -0600

    reduced two array memory allocating

--- skeleton/sampleSkeleton135cube.py
+++ skeleton/sampleSkeleton135cube.py
@@ -7,24 +7,18 @@ from skimage.filters import threshold_otsu
 from skeleton.convOptimize import getSkeletonize3D
 from skeleton.unitwidthcurveskeleton import getShortestPathskeleton
 
-# os.mkdir("/media/pranathi/User Data/subsubVolumeThinned/")
-# os.mkdir("/media/pranathi/User Data/subsubVolumeThresholds/")
-# os.mkdir("/media/pranathi/User Data/subsubVolumeSkeletons/")
-# valid 3D coordinates inside the brain
+
 mask = np.load('/media/pranathi/DATA/maskDownsampled.npy')
 isum = 0; iskpy = iskpx = 560; iskpz = int(0.5 + iskpx * 0.7 / 5.0)
 root = '/media/pranathi/DATA/ii-5016-15-ms-brain_1920/filt/'
 formatOfFiles = 'png'
 f = open("/media/pranathi/DATA/StatsSubVolume.txt", 'w')
 strLists = []
-# list and sort all the files in the given greyscale
-# input root directory
-# which is to be skeletonized
 listOfJpgs = [os.path.join(root, files) for files in os.listdir(root) if formatOfFiles in files]
 listOfJpgs.sort()
 subVolume = np.zeros((19, 17480, 8026), dtype=np.uint8)
 for i in range(9, 799 - 10, iskpz):
-    # print(i)
+    print(i)
     subList = listOfJpgs[i - 9: i + 10]
     count = 0
     for fileName in subList:
@@ -37,19 +31,18 @@ for i in range(9, 799 - 10, iskpz):
                 izd = int(i / 20); ixd = int(k / 140); iyd = int(j / 140)
                 if mask[izd, iyd, ixd]:
                     isum = isum + 1
-                    subSubvolume = subVolume[:, j - 67:j + 68, k - 67: k + 68]
-                    subSubvolume = 255 - subSubvolume
+                    subSubvolume = 255 - subVolume[:, j - 67:j + 68, k - 67: k + 68]
                     interpolatedIm = ndimage.interpolation.zoom(subSubvolume, [5 / 0.7037037, 1, 1], order=2, prefilter=False)
                     t = threshold_otsu(interpolatedIm)
-                    thresh = interpolatedIm > t
+                    interpolatedIm = interpolatedIm > t
                     st = time.time()
-                    thinned = getSkeletonize3D(thresh)
+                    thinned = getSkeletonize3D(interpolatedIm)
                     sh = time.time()
-                    strLists.append("skeletonized %i number of pixels in %0.2f seconds" % (thresh.sum(), sh - st))
+                    strLists.append("skeletonized %i number of pixels in %0.2f seconds" % (interpolatedIm.sum(), sh - st))
                     skeleton = getShortestPathskeleton(thinned)
                     strLists.append("shortest path skeleton in %i number of pixels in %0.2f seconds" % (thinned.sum(), time.time() - sh))
                     np.save("/media/pranathi/DATA/subsubVolumeSkeletons/skeleton_{}_{}_{}.npy".format(i, j, k), skeleton)
-                    np.save("/media/pranathi/DATA/subsubVolumeThresholds/threshold_{}_{}_{}.npy".format(i, j, k), thresh)
+                    np.save("/media/pranathi/DATA/subsubVolumeThresholds/threshold_{}_{}_{}.npy".format(i, j, k), interpolatedIm)
                     l, count = ndimage.measurements.label(skeleton, structure=np.ones((3, 3, 3), dtype=np.uint8))
                     strLists.append("disjoint objects of {} shortest path skeleton subvolume is {}". format(isum, count))
 f.writelines(strLists)
