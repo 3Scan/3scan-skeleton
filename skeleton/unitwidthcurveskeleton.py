0adb2155238721670b8503b99b95850dfe4793ed
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-03-01 11:17:31 -0600

    simplified and corrected code(route through array, crowded region, tuple)

--- skeleton/unitwidthcurveskeleton.py
+++ skeleton/unitwidthcurveskeleton.py
@@ -20,11 +20,10 @@ from skimage.graph import route_through_array
 def _setValenceOfarray(arr):
     template = np.ones((3, 3, 3), dtype=np.uint8)
     template[1, 1, 1] = 0
-
+    arr = np.uint8(arr)
     result = convolve(arr, template, mode='constant', cval=0)
     result[arr == 0] = 0
-    dictOfIndicesAndvalencies = {tuple(item): result[tuple(item)] for item in list(np.transpose(np.nonzero(result)))}
-    return result, dictOfIndicesAndvalencies
+    return result
 
 
 def _intersect(a, b):
@@ -56,7 +55,7 @@ def _setLabelEndMiddlepoints(connNeighbors, nzi):
        between the non zero coordinates is considered
     """
     cNeighbors = int(np.sum(connNeighbors))
-    assert cNeighbors in [1, 2]
+    assert cNeighbors in [2]
 
     if cNeighbors == 2:
         if np.sum((nzi[0] - nzi[1]) ** 2) > 3:
@@ -91,17 +90,10 @@ def _intersectCrowded(a, b):
         return 0
 
 
-def _setLabelCrowdedregion(connNeighborsList):
-    if _intersectCrowded([4], connNeighborsList) == 1:
-        return 5
-    else:
-        return 4
-
-
-def outOfPixBOunds(nearByCoordinate, aShape):
+def outOfPixBounds(nearByCoordinate, aShape):
     onbound = 0
     for index, maxVal in enumerate(aShape):
-        isAtBoundary = nearByCoordinate[index] in [0, maxVal] or nearByCoordinate[index] > maxVal
+        isAtBoundary = nearByCoordinate[index] >= maxVal or nearByCoordinate[index] < 0
         if isAtBoundary:
             onbound = 1
             break
@@ -113,6 +105,7 @@ def outOfPixBOunds(nearByCoordinate, aShape):
 stepDirect = itertools.product((-1, 0, 1), repeat=3)
 listStepDirect = list(stepDirect)
 listStepDirect.remove((0, 0, 0))
+listStepDirect = list(map(np.array, listStepDirect))
 
 
 def _getAllLabelledarray(skeletonIm, valencearray):
@@ -120,87 +113,65 @@ def _getAllLabelledarray(skeletonIm, valencearray):
        label end, middle, joint, crowded points and a
        crowded region as 1, 2, 3, 4 and 5 respectively
     """
-    skeletonImLabel = np.zeros_like(skeletonIm)
+    skeletonImLabel = np.zeros(skeletonIm.shape, dtype=np.uint8)
     skeletonImLabel[valencearray == 1] = 1
     aShape = np.shape(valencearray)
 
-    iterationMaskEndAndMiddle = np.zeros_like(skeletonIm)
-    iterationMaskEndAndMiddle[valencearray == 2] = 1
-    listIterateEndAndMiddle = list(np.transpose(np.nonzero(iterationMaskEndAndMiddle)))
-    for k in listIterateEndAndMiddle:
+    listIterateMiddle = list(np.transpose(np.array(np.where(valencearray == 2))))
+    for k in listIterateMiddle:
         connNeighborsList = []
         connNeighborsIndices = []
         for d in listStepDirect:
-            nearByCoordinate = tuple(k + np.array(d))
-            if outOfPixBOunds(nearByCoordinate, aShape):
+            nearByCoordinate = tuple(k + d)
+            if outOfPixBounds(nearByCoordinate, aShape):
                 continue
             if skeletonIm[nearByCoordinate] == 0:
                 continue
             connNeighbors = skeletonIm[nearByCoordinate]
             connNeighborsList.append(connNeighbors)
             connNeighborsIndices.append(np.array(nearByCoordinate))
-        if connNeighborsList == []:
-            continue
         skeletonImLabel[tuple(k)] = _setLabelEndMiddlepoints(connNeighborsList, connNeighborsIndices)
-    skeletonImLabel[valencearray > 2] = 3
-    skeletonImLabel2 = np.copy(skeletonImLabel)
-    iterationMaskJointAndCrowded = np.zeros_like(skeletonIm)
-    iterationMaskJointAndCrowded[valencearray > 2] = 1
-    listJointAndcrowded = list(np.transpose(np.nonzero(iterationMaskJointAndCrowded)))
+
+    listJointAndcrowded = list(np.transpose(np.array(np.where(valencearray > 2))))
     for k in listJointAndcrowded:
         connNeighborsList = []
         for d in listStepDirect:
-            nearByCoordinate = tuple(k + np.array(d))
-            if outOfPixBOunds(nearByCoordinate, aShape):
+            nearByCoordinate = tuple(k + d)
+            if outOfPixBounds(nearByCoordinate, aShape):
                 continue
-            if skeletonImLabel[nearByCoordinate] != 0:
-                connNeighbors = skeletonImLabel[nearByCoordinate]
-                connNeighborsList.append(connNeighbors)
-        skeletonImLabel2[tuple(k)] = _setLabelJointCrowdedpoints(connNeighborsList)
-    skeletonImLabel3 = np.copy(skeletonImLabel2)
-    iterationMaskCrowdedRegion = np.zeros_like(skeletonImLabel3)
-    iterationMaskCrowdedRegion[skeletonImLabel2 == 4] = 1
-    listCrowdedRegion = list(np.transpose(np.nonzero(iterationMaskCrowdedRegion)))
-    for index, k in enumerate(listCrowdedRegion):
-        connNeighborsList = []
-        for d in listStepDirect:
-            nearByCoordinate = tuple(k + np.array(d))
-            if outOfPixBOunds(nearByCoordinate, aShape):
+            if skeletonImLabel[nearByCoordinate] == 0:
                 continue
-            if skeletonImLabel2[nearByCoordinate] != 0:
-                connNeighbors = skeletonImLabel2[nearByCoordinate]
-                connNeighborsList.append(connNeighbors)
-        skeletonImLabel3[tuple(k)] = _setLabelCrowdedregion(connNeighborsList)
-    listOfLabelledArrays = [skeletonImLabel, skeletonImLabel2, skeletonImLabel3]
+            connNeighbors = skeletonImLabel[nearByCoordinate]
+            connNeighborsList.append(connNeighbors)
+        skeletonImLabel[tuple(k)] = _setLabelJointCrowdedpoints(connNeighborsList)
     assert _intersectAssert(list(set(map(tuple, list(np.transpose(np.nonzero(skeletonImLabel)))))), list(set(map(tuple, list(np.transpose(np.nonzero(skeletonIm)))))))
-    assert _intersectAssert(list(set(map(tuple, list(np.transpose(np.nonzero(skeletonImLabel2)))))), list(set(map(tuple, list(np.transpose(np.nonzero(skeletonIm)))))))
-    assert _intersectAssert(list(set(map(tuple, list(np.transpose(np.nonzero(skeletonImLabel3)))))), list(set(map(tuple, list(np.transpose(np.nonzero(skeletonIm)))))))
-    return np.uint8(skeletonImLabel3), listOfLabelledArrays
+    return skeletonImLabel
 
 
-def _getSourcesOfShortestpaths(dilatedValenceObjectLoc):
-    listNZI = list(set(map(tuple, np.transpose(np.nonzero(dilatedValenceObjectLoc)))))
-    listIndex = [(coord, dilatedValenceObjectLoc[coord]) for coord in listNZI]
+def _getSourcesOfShortestpaths(dilatedValenceObjectLoc, dilatedLabelledObjectLoc):
+    listNZI = list(np.transpose(np.array(np.where(dilatedLabelledObjectLoc == 4))))
+    listIndex = [(coord, dilatedValenceObjectLoc[tuple(coord)]) for coord in listNZI]
     summationList = []
     for value, valence in listIndex:
         distList = []
         for item in listNZI:
-            dist = np.sum((np.array(value) - np.array(item)) ** 2)
+            dist = np.sum(value - np.array(item) ** 2)
             distList.append(dist)
-        summation = sum(distList) / valence
-        summationList.append(summation)
+        summationList.append(sum(distList) / valence)
     src = listNZI[np.argmin(summationList)]
     return src
 
 
 def _getExitsOfShortestpaths(dilatedRegionExits, dilatedLabelledObjectLoc):
     """
-       exit is a end or middle point
+       exit is a end or middle point 26 connected to the crowded region
     """
     a = set(map(tuple, list(np.transpose(np.nonzero(dilatedRegionExits)))))
-    dilatedRegionExits[dilatedRegionExits == 0] = 2
-    dilatedRegionExits[dilatedLabelledObjectLoc == 5] = 0
-    distTransformedIm = ndimage.distance_transform_edt(dilatedRegionExits)
+    dist = np.zeros_like(dilatedRegionExits)
+    dist[dilatedRegionExits != 0] = 1
+    dist[dilatedLabelledObjectLoc == 4] = 0
+    distTransformedIm = ndimage.distance_transform_edt(dist)
+    distTransformedIm = np.uint8(distTransformedIm * distTransformedIm)
     b = np.array(np.where(distTransformedIm <= 3)).T
     b = set(map(tuple, b))
     return list(a & b)
@@ -211,7 +182,9 @@ def _findShortestPathFromCRcenterToexit(valencearray, source, dest):
        dijkstra's shortest path, route through the array across the
        minimum cost path
     """
-    indices, weight = route_through_array(valencearray, source, dest)
+    assert type(source) == tuple
+    assert type(dest) == tuple
+    indices, weight = route_through_array(valencearray, source, dest, fully_connected=True)
     indices = np.array(indices).T
     path = np.zeros_like(valencearray)
     path[indices[0], indices[1], indices[2]] = 1
@@ -220,33 +193,35 @@ def _findShortestPathFromCRcenterToexit(valencearray, source, dest):
 
 def getShortestPathskeleton(skeletonIm):
     se = np.ones((3, 3, 3), dtype=np.uint8)
-    z, m, n = np.shape(skeletonIm)
     # startt = time.time()
     labelInput, noOfObjects = ndimage.measurements.label(skeletonIm, structure=se)
-    skeletonIm = np.lib.pad(skeletonIm, 1, 'constant', constant_values=0)
-    skeletonImNew = np.zeros(skeletonIm.shape, dtype=bool)
-    listOfLabelledArrays = []
-    valencearray, dict1 = _setValenceOfarray(skeletonIm)
+    skeletonImNew = np.zeros(skeletonIm.shape, dtype=np.uint8)
+    valencearray = _setValenceOfarray(skeletonIm)
     if np.sum(valencearray) == 0:
-        return skeletonIm[1:z + 1, 1:m + 1, 1:n + 1]
+        return skeletonIm
     else:
-        skeletonLabelled, listOfLabelledArrays = _getAllLabelledarray(skeletonIm, valencearray)
+        skeletonLabelled = _getAllLabelledarray(skeletonIm, valencearray)
         crowdedRegion = np.zeros_like(skeletonLabelled)
-        crowdedRegion[skeletonLabelled == 5] = 1
+        crowdedRegion[skeletonLabelled == 4] = 1
         label, noOfCrowdedregions = ndimage.measurements.label(crowdedRegion, structure=se)
         if noOfObjects == 1 and np.sum(skeletonIm) > 50 and noOfCrowdedregions == 1:
-            src = _getSourcesOfShortestpaths(valencearray)
-            skeletonImNew[src] = 1
-            # print(" a large number of elements belong to one region")
-            return np.uint8(skeletonImNew[1:z + 1, 1:m + 1, 1:n + 1])
-        if np.max(skeletonLabelled) <= 4:
-            # print("there are no crowded joint points in the image")
-            return skeletonIm[1:z + 1, 1:m + 1, 1:n + 1]
+            src = _getSourcesOfShortestpaths(valencearray, skeletonLabelled)
+            skeletonImNew[tuple(src)] = 1
+            print(" a large number of elements belong to one region")
+            return skeletonImNew
+        if np.max(skeletonLabelled) < 4:
+            print(np.unique(skeletonLabelled))
+            print("there are no crowded regions in the image")
+            return skeletonIm
         else:
-            # print("crowded joint points exist")
+            print(np.unique(skeletonLabelled))
+            print("crowded points exist", noOfCrowdedregions)
+            skeletonImNew[skeletonLabelled < 4] = True
+            skeletonImNew[skeletonLabelled == 0] = False
+            mask = np.logical_and(valencearray == 0, skeletonIm == 1)
+            skeletonImNew[mask == 1] = False  # see if isolated voxels can be removed (answer: yes)
             objectify = ndimage.find_objects(label)
-            exits = np.uint8(np.logical_or(skeletonLabelled == 1, skeletonLabelled == 2))
-            # print(noOfCrowdedregions)
+            exits = np.logical_or(skeletonLabelled == 1, skeletonLabelled == 2)
             for i in range(0, noOfCrowdedregions):
                 loc = objectify[i]
                 zcoords = loc[0]; ycoords = loc[1]; xcoords = loc[2]
@@ -256,20 +231,17 @@ def getShortestPathskeleton(skeletonIm):
                 dilatedLabelledObjectLoc = skeletonLabelled[regionLowerBoundZ: regionUpperBoundZ, regionLowerBoundY: regionUpperBoundY, regionLowerBoundX: regionUpperBoundX]
                 dilatedLabelledObjectLoc[dilatedLabelledObjectLoc == 0] = 255
                 dilatedRegionExits = exits[regionLowerBoundZ: regionUpperBoundZ, regionLowerBoundY: regionUpperBoundY, regionLowerBoundX: regionUpperBoundX]
-                src = _getSourcesOfShortestpaths(dilatedValenceObjectLoc)
+                src = _getSourcesOfShortestpaths(dilatedValenceObjectLoc, dilatedLabelledObjectLoc)
                 dests = _getExitsOfShortestpaths(dilatedRegionExits, dilatedLabelledObjectLoc)
                 for dest in dests:
-                    dilatedLabelledObjectLoc1 = _findShortestPathFromCRcenterToexit(dilatedLabelledObjectLoc, src, dest)
+                    dilatedLabelledObjectLoc1 = _findShortestPathFromCRcenterToexit(dilatedLabelledObjectLoc, tuple(src), dest)
                     skeletonImNew[regionLowerBoundZ: regionUpperBoundZ, regionLowerBoundY: regionUpperBoundY, regionLowerBoundX: regionUpperBoundX] = np.logical_or(skeletonImNew[regionLowerBoundZ: regionUpperBoundZ, regionLowerBoundY: regionUpperBoundY, regionLowerBoundX: regionUpperBoundX], dilatedLabelledObjectLoc1)
-            skeletonImNew[skeletonLabelled < 5] = True
-            skeletonImNew[skeletonLabelled == 0] = False
-            # skeletonImNew[np.logical_and(valencearray == 0, skeletonIm == 1)] = 1  # see if isolated voxels can be removed (answer: yes)
             # stopp = time.time()
             # print("time taken to find the shortest path skeleton is", (stopp - startt), "seconds")
             label_img1, countObjects = ndimage.measurements.label(skeletonIm, structure=np.ones((3, 3, 3), dtype=np.uint8))
             label_img2, countObjectsShorty = ndimage.measurements.label(skeletonImNew, structure=np.ones((3, 3, 3), dtype=np.uint8))
             assert countObjects >= countObjectsShorty
-            return skeletonImNew[1:z + 1, 1:m + 1, 1:n + 1]
+            return skeletonImNew
 
 
 def list_to_dict(skeletonLabelled):
