177dd6f4c242309d0be21bdc738c4d6dad7d56ff
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-03-03 12:50:16 -0600

    fixed the special case cliques of equal distance t o one other

--- skeleton/cliqueRemoving.py
+++ skeleton/cliqueRemoving.py
@@ -31,24 +31,40 @@ def removeCliqueEdges(networkxGraph):
         # different combination of edges in the cliques and their lengths
         for combedges in combEdge:
             subGraphEdgelengths.append([np.sum((np.array(item[0]) - np.array(item[1])) ** 2) for item in combedges])
+        # print(subGraphEdgelengths)
         cliquEdges = []
         # clique edges to be removed are collected here
         # the edges with maximum edge length
         for mainDim, item in enumerate(subGraphEdgelengths):
-            for subDim, length in enumerate(item):
-                if length == max(max(list(zip(*subGraphEdgelengths)))):
-                    print(length, item)
-                    cliquEdges.append(combEdge[mainDim][subDim])
+            if len(set(item)) != 1:
+                for subDim, length in enumerate(item):
+                    if length == max(item):
+                        cliquEdges.append(combEdge[mainDim][subDim])
+            else:
+                specialCase = combEdge[mainDim]
+                # print("specialCase", specialCase)
+                diffOfEdges = []
+                for numSpcledges in range(0, 3):
+                    l1 = list(specialCase[numSpcledges][0]); l2 = list(specialCase[numSpcledges][1])
+                    diffOfEdges.append([i - j for i, j in zip(l1, l2)])
+                # print("diffOfEdges", diffOfEdges)
+                for index, val in enumerate(diffOfEdges):
+                    if val[1] == 0:
+                        subDim = index
+                        # print(subDim)
+                        break
+                cliquEdges.append(combEdge[mainDim][subDim])
         networkxGraphAfter.remove_edges_from(cliquEdges)
         disjointGraphsAfter = len(list(nx.connected_component_subgraphs(networkxGraphAfter)))
-        print("number of graphs in the output networkxGraph after cliques are removed is", disjointGraphsAfter)
+        # print("number of graphs in the output networkxGraph after cliques are removed is", disjointGraphsAfter)
         assert networkxGraph.number_of_edges() >= networkxGraphAfter.number_of_edges()
-        print("time taken to remove cliques is %0.2f seconds" % (time.time() - startt))
+        # print("time taken to remove cliques is %0.2f seconds" % (time.time() - startt))
+        assert disjointGraphsAfter == disjointGraphsBefore
         if disjointGraphsBefore == disjointGraphsAfter:
-            print("graph changed to remove cliques")
+            # print("graph changed to remove cliques")
             return networkxGraphAfter.copy()
         else:
-            print("graph unchanged")
+            # print("graph unchanged")
             return networkxGraph
 
 
