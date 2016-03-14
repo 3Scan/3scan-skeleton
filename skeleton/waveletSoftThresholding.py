1f7672c2d0965c41bfbd3f9879e61451a55aa831
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-03-14 15:14:25 -0500

    equalise histogram and improvise otsu's code

--- skeleton/waveletSoftThresholding.py
+++ skeleton/waveletSoftThresholding.py
@@ -198,22 +198,14 @@ def plot():
 
 
 def histogramEqualize(subSubvolume):
-    hist, bins = np.histogram(subSubvolume.flatten(), 256, [0, 256])
+    l1 = [y for y in subSubvolume.ravel() if y != 0]
+    hist, bins = np.histogram(l1, 256, [0, 256])
     cdf = hist.cumsum()
-    cdf_normalized = cdf * hist.max() / cdf.max()
-    plt.plot(cdf_normalized, color='b')
-    plt.hist(subSubvolume.flatten(), 256, [0, 256], color='r')
-    plt.xlim([0, 256])
-    plt.legend(('cdf', 'histogram'), loc='upper left')
-    plt.show()
     cdf_m = np.ma.masked_equal(cdf, 0)
     cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
     cdf = np.ma.filled(cdf_m, 0).astype('uint8')
     subSubvolume2 = cdf[subSubvolume]
-    plt.hist(subSubvolume2.flatten(), 256, [0, 256], color='r')
-    plt.xlim([0, 256])
-    plt.legend(('cdf', 'histogram'), loc='upper left')
-    plt.show()
+    return subSubvolume2
 
 
 def avgimprovisedOtsu(subVolumeCopy):
@@ -226,6 +218,26 @@ def avgimprovisedOtsu(subVolumeCopy):
     t = threshold_otsu(np.array(l1))
     print("avg value improvised otsu threshold", t)
     o = subVolumeCopy > t
+    return o, t
+
+
+def avgimprovisedequalisedOtsu(subVolumeCopy):
+    histData, bins = np.histogram(subVolumeCopy.flatten(), 256, [0, 256])
+    sumation = np.sum(histData * bins[0:-1])
+    weight = np.sum(histData)
+    avg = sumation / weight
+    subVolumeCopy[subVolumeCopy < int(avg)] = 0
+    l1 = [y for y in subVolumeCopy.ravel() if y != 0]
+    hist, bins = np.histogram(l1, 256, [0, 256])
+    cdf = hist.cumsum()
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+    subSubvolume2 = cdf[subVolumeCopy]
+    l1 = [y for y in subVolumeCopy.ravel() if y != 0]
+    t = threshold_otsu(np.array(l1))
+    print("avg value improvised otsu threshold", t)
+    o = subSubvolume2 > t
     return o, t
 
 
