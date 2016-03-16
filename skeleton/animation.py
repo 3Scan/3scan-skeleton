002e60f3adf9422f25f96e048d8027de6a2e2d31
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-03-16 15:44:54 -0500

    camera reset after each 10 degree rotation

--- skeleton/animation.py
+++ skeleton/animation.py
@@ -7,15 +7,16 @@ e.start()
 s = e.new_scene()
 threshold = np.load(input("enter a path to thresholded volume"))
 skeleton = np.load(input("enter a path to skeleton you want to superimpose over"))
-l = mlab.contour3d(np.uint8(threshold)).actor.property.representation = 'points'
-l.contours.Auto contours = 'off'
+mlab.contour3d(np.uint8(threshold), contours=[108.50]).actor.property.representation = 'points'
 mlab.contour3d(np.uint8(skeleton), colormap='gray')
 mlab.options.offscreen = True
 # Make an animation:
 for i in range(36):
     # Rotate the camera by 10 degrees.
     s.scene.camera.azimuth(10)
-
+    # Resets the camera clipping plane so everything fits and then
+    # renders.
+    s.scene.reset_zoom()
     # Save the scene. magnification=2 gives saves as an image when seen in fullscreen
     mlab.savefig("anim%d.png" % i, magnification=2)
 
