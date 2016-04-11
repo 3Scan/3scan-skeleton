dd0b4da7b23ac703fdc5cfa024a21a80706f47ee
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-04-11 17:25:37 -0500

    writing less stats

--- skeleton/runscripts/parallelProcessingStats.py
+++ skeleton/runscripts/parallelProcessingStats.py
@@ -3,47 +3,30 @@ import time
 import multiprocessing
 from multiprocessing import Pool
 import numpy as np
-from skeleton.runscripts.thin3DVolumeRV import getSkeleton3D
 from skeleton.runscripts.segmentLengthsRV import getSegmentStats
 
 
 def convert(tupValList):
     for npy in tupValList:
-        interpolatedIm = np.load(npy)
-        npy = npy.replace('media', 'home')
-        npy = npy.replace('User Data/', '')
-        pathSkel = npy.replace('threshold', 'skeleton')
-        skeleton = getSkeleton3D(interpolatedIm)
-        np.save(pathSkel, skeleton)
-        path = (npy.replace('threshold', 'stat')).replace('npy', 'txt')
-        f = open(path, 'w')
-        avgBranchingIndex, numSegments, totalLength, totalTortuosity, cyclesTot = getSegmentStats(skeleton)
-        percentVasc = np.sum(interpolatedIm) / totalSize
-        d = [str(percentVasc) + "\n", str(avgBranchingIndex) + "\n", str(numSegments) + "\n", str(totalLength) + "\n", str(totalTortuosity) + "\n" + str(cyclesTot) + "\n"]
+        path = (npy.replace('skeleton', 'stat')).replace('npy', 'txt')
+        f = open(path, 'a')
+        dist, T1, T2 = getSegmentStats(np.load(npy))
+        d = [str(dist) + "\n", str(T1) + "\n", str(T2) + "\n"]
         f.writelines(d)
         f.close()
 
 
 if __name__ == '__main__':
     totalSize = 2460375.0
-    root = '/media/pranathi/User Data/subsubVolumethresholdNew_28/'
+    root = '/home/pranathi/subsubVolumeskeletonNew_28/'
     formatOfFiles = 'npy'
     listOfNpys = [os.path.join(root, files) for files in os.listdir(root) if formatOfFiles in files]
     listOfNpys.sort()
-    listOfNpys.remove(root + 'threshold_460_10007_7087.npy')
-    listOfNpys.remove(root + 'threshold_630_8871_7087.npy')
-    listN = []
-    for npy in listOfNpys:
-        npyN = npy.replace('media', 'home')
-        npyN = npyN.replace('User Data/', '')
-        pathSkel = npyN.replace('threshold', 'skeleton')
-        if os.path.exists(pathSkel) == 0:
-            listN.append(npy)
     numProcessors = multiprocessing.cpu_count()
-    chunkSize = int(len(listN) / numProcessors)
+    chunkSize = int(len(listOfNpys) / numProcessors)
     poolLists = []
-    for i in range(0, len(listN), chunkSize + 1):
-        poolLists.append(listN[i: i + chunkSize + 1])
+    for i in range(0, len(listOfNpys), chunkSize + 1):
+        poolLists.append(listOfNpys[i: i + chunkSize + 1])
     startt = time.time()
     pool = Pool(processes=numProcessors)
     pool.map(convert, poolLists)
