cb82d6cc1ef03cbbd502adec2b0401a1079cc512
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-05-05 21:06:40 -0500

    12 lookup tables parallel processing

--- skeleton/generateLookupTable.py
+++ skeleton/generateLookupTable.py
@@ -1,6 +1,8 @@
 import numpy as np
-from rotationalOperators import firstSubIter, secondSubIter, thirdSubIter, fourthSubIter, fifthSubIter, sixthSubIter
+from rotationalOperators import secondSubIter, thirdSubIter, fourthSubIter, fifthSubIter, sixthSubIter
 from rotationalOperators import seventhSubIter, eighthSubIter, ninthSubIter, tenthSubIter, eleventhSubIter, twelvethSubIter
+import multiprocessing
+from multiprocessing import Pool
 
 """
    reference paper
@@ -8,15 +10,15 @@ from rotationalOperators import seventhSubIter, eighthSubIter, ninthSubIter, ten
 """
 
 
-def generateLookuparray(stop, iterationNumber):
+def generateLookuparray(iterationNumber):
     """
        to generate a look up aray the all the 2 ** 26 possible configurations are
        checked if they satisy one of the 14 templates
        iterationNumber is the rotation of the cube into one of the directions
        refer to the paper Parallel 3D thinning algorithm with 12 directions for more details
     """
+    iteration = directionList[iterationNumber]
     lookuparray = np.zeros(stop, dtype=bool)
-    print(iterationNumber)
     for item in range(0, stop):
         neighborValues = [(item >> digit) & 0x01 for digit in range(26)]
         neighborValues.insert(13, 1)
@@ -24,7 +26,8 @@ def generateLookuparray(stop, iterationNumber):
         if np.sum(neighborValues) == 1:
             lookuparray[item] = 0
         else:
-            neighborValues = iterationNumber(neighborMatrix)
+            neighborValues = iteration(neighborMatrix)
+            listVal = [val * (2 ** index) for index, val in enumerate(neighborValues)]
             a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = tuple(neighborValues)
             shouldVoxelBedeleted = (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & p & (d | e | f | m | n | u | v | w | g | h | i | o | q | x | y | z)) | \
                                    (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & v & (r | s | t | j | k | l | m | n | u | w | o | p | q | x | y | z)) | \
@@ -40,16 +43,16 @@ def generateLookuparray(stop, iterationNumber):
                                    (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(l) & ~(r) & ~(s) & ~(t) & ~(e) & ~(f) & ~(h) & ~(i) & o & y) | \
                                    (~(a) & ~(b) & ~(c) & ~(j) & ~(k) & ~(r) & ~(s) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & w & y) | \
                                    (~(a) & ~(b) & ~(c) & ~(d) & ~(e) & ~(f) & ~(g) & ~(h) & ~(i) & ~(k) & ~(l) & ~(s) & ~(t) & u & y)
+            item = sum(listVal)
             lookuparray[item] = shouldVoxelBedeleted
-    return lookuparray
-
-
-directionList = [firstSubIter, secondSubIter, thirdSubIter, fourthSubIter,
-                 fifthSubIter, sixthSubIter, seventhSubIter, eighthSubIter,
-                 ninthSubIter, tenthSubIter, eleventhSubIter, twelvethSubIter]
+    np.save("lookuparray%i.npy" % (iterationNumber + 2), lookuparray)
 
 if __name__ == '__main__':
-    for i, item in enumerate(directionList):
-        lookuparray = generateLookuparray(2 ** 26, item)
-        np.save("lookuparray%i.npy" % (i + 1), lookuparray)
+    numProcessors = multiprocessing.cpu_count() - 2
+    directionList = [secondSubIter, thirdSubIter, fourthSubIter,
+                     fifthSubIter, sixthSubIter, seventhSubIter, eighthSubIter,
+                     ninthSubIter, tenthSubIter, eleventhSubIter, twelvethSubIter]
+    stop = 2 ** 26
+    pool = Pool(numProcessors)
+    pool.map(generateLookuparray, list(range(0, 11)))
 
