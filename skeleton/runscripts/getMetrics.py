f0e352d051756c555cf13658954dbb53bde9698d
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-05-26 18:52:52 -0700

    getMetrics runscript corrected wrt watershed o/p

--- skeleton/runscripts/getMetrics.py
+++ skeleton/runscripts/getMetrics.py
@@ -1,6 +1,8 @@
 from scipy import ndimage
 
-from KESMAnalysis.imgtools import loadStack
+from KESMAnalysis.imgtools import loadStack, saveStack
+from KESMAnalysis.pipeline.pipelineComponents import watershedMarker
+
 from skeleton.thin3DVolume import getThinned3D
 from skeleton.unitwidthcurveskeleton import getShortestPathSkeleton
 from skeleton.segmentStats import getSegmentStats
@@ -9,19 +11,23 @@ from skeleton.segmentStats import getSegmentStats
 filePath = input("please enter a root directory where your median filtered 2D slices are----")
 stack = loadStack(filePath)
 
-# load aspect ratio to make the 3D volume isotropic
-aspectRatio = input("please enter resolution of a voxel in 3D with resolution in z followed by y and x")
+# load aspect ratio to make the 3D volume isotropic using quadratic interpolation
+aspectRatio = input("please enter resolution of a voxel in 3D with resolution in x followed by y and z (spaces in between)")
 aspectRatio = [float(item) for item in aspectRatio.split(' ')]
 stack = ndimage.interpolation.zoom(stack, zoom=aspectRatio, order=2, prefilter=False)
 
 # binarize using 3D watershed transform
-binaryVol = binarize(stack)
-
+binaryVol = watershedMarker(stack)
+binaryVol[binaryVol == 255] = 1
+binaryVol = binaryVol.astype(bool)
 # thin binarized volume of vessels
 thinnedVol = getThinned3D(binaryVol)
 
 # decluster thinned volume
 skeletonVol = getShortestPathSkeleton(thinnedVol)
+
+# save the skeleton volume
+saveStack(skeletonVol, filePath + "/skeleton")
 
 # vectorize and find metrics
 segmentCountdict, segmentLengthdict, segmentTortuositydict, totalSegments, typeGraphdict, avgBranching, endP, branchP, segmentContractiondict, segmentHausdorffDimensiondict = getSegmentStats(skeletonVol)
