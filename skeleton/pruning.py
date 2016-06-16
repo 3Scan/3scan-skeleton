2b566f6a5843583bfd87b65d870b623131085c68
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-06-16 13:41:55 -0700

    networkx path finding and pruning branches

--- skeleton/pruning.py
+++ skeleton/pruning.py
@@ -1,10 +1,10 @@
 import time
 import numpy as np
-from scipy.ndimage.filters import convolve
 from scipy import ndimage
 import networkx as nx
 from skeleton.networkxGraphFromarray import getNetworkxGraphFromarray
 from skeleton.cliqueRemoving import removeCliqueEdges
+import itertools
 # from skeleton.thin3DVolume import getThinned3D
 # from skeleton.unitwidthcurveskeleton import getShortestPathSkeleton
 
@@ -14,19 +14,36 @@ from skeleton.cliqueRemoving import removeCliqueEdges
     https://en.wikipedia.org/wiki/Pruning_(morphology)
     http://www.mathworks.com/matlabcentral/answers/88284-remove-the-spurious-edge-of-skeleton?requestedDomain=www.mathworks.com
 """
-template = np.ones((3, 3, 3), dtype=np.uint8)
-template[1, 1, 1] = 0
 
 
-def bwmorph(skel, str):
-    valencearray = convolve(np.uint8(skel), template, mode='constant', cval=0)
-    valencearray[skel == 0] = 0
-    mask = np.zeros_like(skel)
-    if str is 'endpoints':
-        mask[valencearray == 1] = 1
+def _getChessboardDist(path, cycle=0):
+    """
+       finds distance between points in a given path
+       if it is a cycle then distance from last coordinate
+       to first coordinate in the path is added to the
+       distance list (since a cycle contains this edge as
+       the last edge in its path)
+    """
+    distList = []
+    if cycle:
+        for index, item in enumerate(path):
+            x1, y1, z1 = item
+            if index + 1 < len(path):
+                item2 = path[index + 1]
+            elif index + 1 == len(path):
+                item2 = path[0]
+            x2, y2, z2 = item2
+            dist = max(abs(x1 - x2), abs(y1 - y2), abs(z1 - z2))
+            distList.append(dist)
     else:
-        mask[valencearray >= 3] = 1
-    return mask
+        for index, item in enumerate(path[:-1]):
+            x1, y1, z1 = item
+            item2 = path[index + 1]
+            x2, y2, z2 = item2
+            dist = max(abs(x1 - x2), abs(y1 - y2), abs(z1 - z2))
+            distList.append(dist)
+    return sum(distList)
+
 
 start_prune = time.time()
 filePath = input("please enter a root directory where your 3D input is---")
@@ -35,27 +52,28 @@ skel = np.load(filePath)
 label_img1, countObjects = ndimage.measurements.label(skel, structure=np.ones((3, 3, 3), dtype=np.uint8))
 networkxGraph = getNetworkxGraphFromarray(skel)
 networkxGraph = removeCliqueEdges(networkxGraph)
-# list of disjointgraphs
-visitedSources = []
-disjointGraphs = list(nx.connected_component_subgraphs(networkxGraph))
 ndd = nx.degree(networkxGraph)
-listEndIndices = [np.array(k) for (k, v) in ndd.items() if v == 1]
+listEndIndices = [k for (k, v) in ndd.items() if v == 1]
 listBranchIndices = [k for (k, v) in ndd.items() if v != 2 and v != 1]
-listIndices = list(np.transpose(np.array(np.where(skel != 0))))
-skelD = np.copy(skel)
-for endPoint in listEndIndices:
+branchendpoints = listBranchIndices + listEndIndices
+Dmask = np.zeros_like(skel)
+maskPresent = np.zeros_like(skel)
+D = np.zeros(skel.shape)
+count = 0
+for sourceOnTree, item in itertools.product(listEndIndices, listBranchIndices):
     listOfBranchDists = []
-    D = np.zeros(skel.shape)
-    for item in listIndices:
-        dist = np.sum(np.square(endPoint - item))
-        tupItem = tuple(item)
-        D[tupItem] = dist
-        if tupItem in listBranchIndices:
-            listOfBranchDists.append(dist)
-    skelD[D < min(listOfBranchDists)] = 0
-E = bwmorph(skelD, 'endpoints')
-template = np.ones((3, 3, 3), dtype=np.uint8)
-skelNew = ndimage.morphology.binary_dilation(skelD, structure=template, iterations=1, mask=E)
-label_img2, countObjectsPruned = ndimage.measurements.label(skelNew, structure=np.ones((3, 3, 3), dtype=np.uint8))
-assert countObjects == countObjectsPruned, "Number of disconnected objects in pruned skeleton {} is greater than input objects {}".format(countObjectsPruned, countObjects)
+    for simplePath in nx.all_simple_paths(networkxGraph, source=sourceOnTree, target=item):
+        if sum([1 for point in simplePath if point in branchendpoints]) == 2:
+            curveLength = np.sqrt(np.sum(np.square(np.array(sourceOnTree) - np.array(item))))
+            D[item] = curveLength
+            listOfBranchDists.append(curveLength)
+    if listOfBranchDists != []:
+        count += 1
+        Dmask[D < min(listOfBranchDists)] = 1
+        print(count, Dmask.sum())
+skelD = skel - Dmask
+label_img2, countObjectsPruned = ndimage.measurements.label(skel, structure=np.ones((3, 3, 3), dtype=np.uint8))
 print("time taken is %0.3f seconds" % (time.time() - start_prune))
+assert countObjects == countObjectsPruned, "Number of disconnected objects in pruned skeleton {} is greater than input objects {}".format(countObjectsPruned, countObjects)
+
+
