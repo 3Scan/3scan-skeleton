3bf0d470f73cd4d4d6b9238e6e5c4120a97f5e26
pranathivemuri <pranathi93.vemuri@gmail.com>
2016-06-16 17:21:24 -0700

    tests for pruning added

--- skeleton/pruning.py
+++ skeleton/pruning.py
@@ -13,35 +13,39 @@ from skeleton.segmentStats import _getDistanceBetweenPointsInpath
     http://www.mathworks.com/matlabcentral/answers/88284-remove-the-spurious-edge-of-skeleton?requestedDomain=www.mathworks.com
 """
 
-start_prune = time.time()
-filePath = input("please enter a root directory where your 3D input is---")
-# skel = getShortestPathSkeleton(getThinned3D(np.load(filePath)))
-skel = np.load(filePath)
-label_img1, countObjects = ndimage.measurements.label(skel, structure=np.ones((3, 3, 3), dtype=np.uint8))
-networkxGraph = getNetworkxGraphFromarray(skel)
-networkxGraph = removeCliqueEdges(networkxGraph)
-# list of disjointgraphs
-visitedSources = []
-disjointGraphs = list(nx.connected_component_subgraphs(networkxGraph))
-ndd = nx.degree(networkxGraph)
-listEndIndices = [k for (k, v) in ndd.items() if v == 1]
-listBranchIndices = [k for (k, v) in ndd.items() if v != 2 and v != 1]
-listIndices = list(np.transpose(np.array(np.where(skel != 0))))
-skelD = np.copy(skel)
-for endPoint in listEndIndices:
-    listOfBranchDists = []
-    D = np.zeros(skel.shape)
-    for item in listIndices:
-        tupItem = tuple(item)
-        tupEnd = tuple(endPoint)
-        if nx.has_path(networkxGraph, tupEnd, tupItem):
-            simplePath = next(nx.all_simple_paths(networkxGraph, source=tupEnd, target=tupItem), 6)
-            dist = _getDistanceBetweenPointsInpath(simplePath)
-            D[tupItem] = dist
-            if tupItem in listBranchIndices:
-                listOfBranchDists.append(dist)
-    skelD[D < min(listOfBranchDists)] = 0
 
-label_img2, countObjectsPruned = ndimage.measurements.label(skelD, structure=np.ones((3, 3, 3), dtype=np.uint8))
-print("time taken is %0.3f seconds" % (time.time() - start_prune))
-assert countObjects == countObjectsPruned, "Number of disconnected objects in pruned skeleton {} is greater than input objects {}".format(countObjectsPruned, countObjects)
+def getPrunedSkeleton(skel):
+    print(np.unique(skel))
+    start_prune = time.time()
+    label_img1, countObjects = ndimage.measurements.label(skel, structure=np.ones((3, 3, 3), dtype=np.uint8))
+    networkxGraph = getNetworkxGraphFromarray(skel)
+    networkxGraph = removeCliqueEdges(networkxGraph)
+    ndd = nx.degree(networkxGraph)
+    listEndIndices = [k for (k, v) in ndd.items() if v == 1]
+    listBranchIndices = [k for (k, v) in ndd.items() if v != 2 and v != 1]
+    listIndices = list(np.transpose(np.array(np.where(skel != 0))))
+    skelD = np.copy(skel)
+    for endPoint in listEndIndices:
+        listOfBranchDists = []
+        D = np.zeros(skel.shape)
+        for item in listIndices:
+            tupItem = tuple(item)
+            tupEnd = tuple(endPoint)
+            if nx.has_path(networkxGraph, tupEnd, tupItem):
+                simplePath = next(nx.all_simple_paths(networkxGraph, source=tupEnd, target=tupItem))
+                dist = _getDistanceBetweenPointsInpath(simplePath)
+                D[tupItem] = dist
+                if tupItem in listBranchIndices:
+                    listOfBranchDists.append(dist)
+        if listOfBranchDists != []:
+            skelD[D < min(listOfBranchDists)] = 0
+
+    label_img2, countObjectsPruned = ndimage.measurements.label(skelD, structure=np.ones((3, 3, 3), dtype=np.uint8))
+    print("time taken is %0.3f seconds" % (time.time() - start_prune))
+    assert countObjects == countObjectsPruned, "Number of disconnected objects in pruned skeleton {} is greater than input objects {}".format(countObjectsPruned, countObjects)
+    return skelD
+
+
+if __name__ == '__main__':
+    filePath = input("please enter a root directory where your 3D input is---")
+    skel = np.load(filePath)
---
+++ tests/testPruning.py
@@ -0,0 +1,56 @@
+import numpy as np
+from scipy import ndimage
+from skeleton.pruning import getPrunedSkeleton
+from skeleton.unitwidthcurveskeleton import getShortestPathSkeleton
+from skeleton.thin3DVolume import getThinned3D
+
+
+def getRectangle(w=10, h=6, d=6):
+    return np.ones((w, h, d), dtype=bool)
+
+
+def getRectangleNoise(w=10, h=6, d=6):
+    rect = np.zeros((w + 2, h + 2, d + 2), dtype=bool)
+    rect[1: w + 1, 1: h + 1, 1: d + 1] = 1
+    rect[int((w / 2) + 1), int((h / 2) + 1), 1] = 1
+    return rect
+
+
+def getRandomBlob():
+    from scipy.spatial import ConvexHull
+    xs = np.random.uniform(-1, 1, size=5)
+    ys = np.random.uniform(-1, 1, size=5)
+    zs = np.random.uniform(-1, 1, size=5)
+
+    xyzs = list(zip(xs, ys, zs))
+
+    hullz = ConvexHull(xyzs)
+
+    xf, yf, zf = np.mgrid[-1:1:50j, -1:1:50j, -1:1:50j]
+    i = np.ones(xf.shape, dtype=bool)
+    for x, y, z, c in hullz.equations:
+        mask = (xf * x) + (yf * y) + (zf * z) - c < 0
+        i[mask] = 0
+    i = i.astype(bool)
+    return i
+
+
+def checkAlgorithmSinglePixeled(image):
+    print("sum before", np.sum(image))
+    newImage = getPrunedSkeleton(getShortestPathSkeleton(getThinned3D(image)))
+    label_img, countObjectsn = ndimage.measurements.label(newImage, structure=np.ones((3, 3, 3), dtype=np.uint8))
+    label_img, countObjects = ndimage.measurements.label(image, structure=np.ones((3, 3, 3), dtype=np.uint8))
+    print(countObjects, countObjectsn, np.sum(newImage))
+    assert (countObjectsn == countObjects)
+
+
+def test_rectangle():
+    checkAlgorithmSinglePixeled(getRectangle())
+
+
+def test_rectangleNoise():
+    checkAlgorithmSinglePixeled(getRectangleNoise())
+
+
+def test_randomBlob():
+    checkAlgorithmSinglePixeled(getRandomBlob())
