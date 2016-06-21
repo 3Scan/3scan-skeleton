1d522a5300bbd600c661fdae24081a730a38ae5d
Mike Pesavento <mike@3scan.com>
2016-06-21 15:59:28 -0700

    minor refactoring of pruning loops

--- skeleton/pruning.py
+++ skeleton/pruning.py
@@ -13,6 +13,11 @@ from skeleton.cliqueRemoving import removeCliqueEdges
 
 
 def getPrunedSkeleton(skel, cutoff=9):
+    """
+    take 3D ND-array of skeletonized data and prune branches shorter than length 'cutoff'
+    """
+    # NOTE MJP: we should not be using the full 3D array when we can avoid it, since a sparse
+    #    representation will be more efficient
     start_prune = time.time()
     label_img1, countObjects = ndimage.measurements.label(skel, structure=np.ones((3, 3, 3), dtype=np.uint8))
     networkxGraph = getNetworkxGraphFromarray(skel)
@@ -21,22 +26,37 @@ def getPrunedSkeleton(skel, cutoff=9):
     listEndIndices = [k for (k, v) in ndd.items() if v == 1]
     listBranchIndices = [k for (k, v) in ndd.items() if v != 2 and v != 1]
     skelD = np.copy(skel)
-    branchendpoints = listEndIndices + listBranchIndices
     for endPoint in listEndIndices:
         for item in listBranchIndices:
             tupItem = tuple(item)
             tupEnd = tuple(endPoint)
-            if nx.has_path(networkxGraph, tupEnd, tupItem):
+            if nx.has_path(networkxGraph, tupEnd, tupItem):  # is it on the same subgraph
                 simplePaths = list(nx.all_simple_paths(networkxGraph, source=tupEnd, target=tupItem, cutoff=cutoff))
                 for simplePath in simplePaths:
-                    if sum([1 for point in simplePath if point in branchendpoints]) == 2:
-                        for pointsSmallBranches in simplePath[:-1]:
-                            skelD[pointsSmallBranches] = 0
+                    if countBranchPointsOnSimplePath(simplePath, listBranchIndices) == 1:
+                        skelD = removeNodesOnPath(simplePath, skelD)
+
     label_img2, countObjectsPruned = ndimage.measurements.label(skelD, structure=np.ones((3, 3, 3), dtype=np.uint8))
     print("time taken is %0.3f seconds" % (time.time() - start_prune))
     print(countObjects, countObjectsPruned)
     # assert countObjects == countObjectsPruned, "Number of disconnected objects in pruned skeleton {} is greater than input objects {}".format(countObjectsPruned, countObjects)
     return skelD
+
+
+def countBranchPointsOnSimplePath(simplePath, listBranchIndices):
+    """
+    given a list of nodes as a simplePath, return how many branch points there are on that path
+    """
+    return sum([1 for point in simplePath if point in listBranchIndices])
+
+
+def removeNodesOnPath(simplePath, skelD):
+    """
+    remove the nodes on simplePath from 3D volume skelD
+    """
+    for pointsSmallBranches in simplePath[:-1]:
+        skelD[pointsSmallBranches] = 0
+    return skelD  # we are doing removal in place, so technically dont need to return it
 
 
 if __name__ == '__main__':
